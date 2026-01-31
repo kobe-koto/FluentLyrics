b0f9ddd3111c8f3e8e9be6b621847e2e2b5d3bf0
kobe koto <91669733+kobe-koto@users.noreply.github.com>
2026-01-31 17:18:51 +0800

    reorder providerPriority to move lrclib to the last position by default

--- lib/constants/app_defaults.dart
+++ lib/constants/app_defaults.dart
@@ -12,10 +12,10 @@ class AppDefaults {
   static const double fontSize = 36.0;
   static const double inactiveScale = 0.85;
   static const List<LyricProviderType> providerPriority = [
-    LyricProviderType.lrclib,
     LyricProviderType.musixmatch,
     LyricProviderType.netease,
     LyricProviderType.qqmusic,
+    LyricProviderType.lrclib,
   ];
   static const int enabledProviderCount = 3;
   static const bool cacheEnabled = true;
