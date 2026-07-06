c86fada5a4554e31a7fd423e29824c8c878c799a
kobe koto <91669733+kobe-koto@users.noreply.github.com>
2026-07-06 23:20:30 +0800

    migrate android gradle configs

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,10 +1,8 @@
 import java.util.Properties
 import java.io.FileInputStream
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("com.android.application")
-    id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -21,7 +19,7 @@ android {
 
     kotlin {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
+            jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
         }
     }
 
